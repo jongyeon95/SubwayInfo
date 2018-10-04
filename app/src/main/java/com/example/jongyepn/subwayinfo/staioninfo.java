31a744b4e73f2c850ab374ea05dfed54e84e8735
whddus9509 <whddus9509@naver.com>
2018-10-04 14:11:37 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jongyepn/subwayinfo/staioninfo.java
@@ -0,0 +1,13 @@
+package com.example.jongyepn.subwayinfo;
+
+import android.app.AppComponentFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class staioninfo extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      setContentView(R.layout.sationinfo);
+    }
+
+}
